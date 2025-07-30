830a7eed58b01de71299cd2b4502cb91ec57faf5
yagocam <yagocam5@gmail.com>
2025-07-30 14:56:32 -0300

    fix: allowed origin

--- src/main/java/com/sop/backend/config/SecurityConfig.java
+++ src/main/java/com/sop/backend/config/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
