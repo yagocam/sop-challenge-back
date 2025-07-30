3a7831e62bc4eab7f620de2005ecf0d7cce7e2b0
yagocam <yagocam5@gmail.com>
2025-07-30 11:58:26 -0300

    fix: fixing auth

--- src/main/java/com/sop/backend/services/JwtService.java
+++ src/main/java/com/sop/backend/services/JwtService.java
@@ -22,7 +22,6 @@ public class JwtService {
     public JwtService(@Value("${jwt.secret}") String secret) {
         this.secret = secret;
     }
-    public static final String SECRET = "9D0EB6B1C2E1FAD0F53A248F6C3B5E4E2F6D8G3H1I0J7K4L1M9N2O3P5Q0R7S9T1U4V2W6X0Y3Z";
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userName);
